afb71b4d9ef5464e38d4bf96e1cb43876c82ea9f
JackMac10 <macdonaldjackson0@gmail.com>
2024-05-01 18:34:22 -0700

    sass styling to photoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,8 +5,8 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
   return (
-    <div className="App">
-      <img src={imageSource}/>
+    <div className="photo-list__item">
+      <img  src={imageSource}/>
         <img src={profile}/>
         <p>{username}</p>
         <p>{location.city}, {location.country}</p>
