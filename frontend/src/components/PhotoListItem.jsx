c130e6664a62fb95c4173ae62690480e9c72fa04
JackMac10 <macdonaldjackson0@gmail.com>
2024-05-01 17:43:44 -0700

    photolistItems added and refactored to use props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <div className="App">
+      <img src={imageSource}/>
+        <img src={profile}/>
+        <p>{username}</p>
+        <p>{location.city}, {location.country}</p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
