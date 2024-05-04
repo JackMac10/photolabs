6f9bd8ee2e033ce1f1fe0ee5dcdb7e2fe10023ac
JackMac10 <macdonaldjackson0@gmail.com>
2024-05-03 17:34:18 -0700

    like button added

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,22 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [displayAlert, setDisplayAlert] = useState(false);
+
+  const handleToggleSelect = () => {
+    console.log('liked')
+    setIsSelected(prevState => !prevState);
+  };
+
+  const handleToggleAlert = () => {
+    setDisplayAlert(prevState => !prevState);
+  };
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={handleToggleSelect}>
+      <FavIcon selected={isSelected} displayAlert={displayAlert} />
+
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
   return (
     <div className="photo-list__item">
+      <PhotoFavButton/>
       <img className="photo-list__photo" src={imageSource}/>
       <div className="details">
         <img className="profile-pic" src={profile}/>
